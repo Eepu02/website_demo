e5e2db2c5514a577ee3b215c4b8b194cbb7ba2e0
Eepu02 <49359558+Eepu02@users.noreply.github.com>
2022-03-21 17:50:46 +0200

    da

--- photo.js
+++ photo.js
@@ -10,6 +10,8 @@ var nextImg = "/assets/" + nextNumber + ".jpg";
 console.log(nextImg); */
 
 const html = document.documentElement;
+
+// for easy future referencing
 const canvas = document.querySelector(".product-slide");
 const context = canvas.getContext('2d');
 const frameCount = 239;
@@ -23,16 +25,20 @@ canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
 const img = new Image();
+
+// Set source to 1st frame
 img.src = currentFrame(1);
+
 const scale = Math.min(canvas.width / img.width, canvas.height / img.height);
 const centerShift_x = (canvas.width - img.width * scale) / 2;
 const centerShift_y = (canvas.height - img.height * scale) / 2; 
-//console.log(scale);
 
+// Load first image on page load
 img.onload = function() {
     context.drawImage(img, 0, 0, img.width, img.height, centerShift_x, centerShift_y, img.width * scale, img.height * scale);
 }
 
+// Updates img path and draws updated image
 const updateImage = index => {
     img.src = currentFrame(index);
     context.drawImage(img, 0, 0, img.width, img.height, centerShift_x, centerShift_y, img.width * scale, img.height * scale);
@@ -49,6 +55,7 @@ window.addEventListener('scroll', () => {
     requestAnimationFrame(() => updateImage(frameIndex + 1))
   });
 
+// Loads images into memory for faster access
 const preloadImages = () => {
     for(let i = 0; i < frameCount; i++) {
         const img = new Image();
@@ -57,7 +64,3 @@ const preloadImages = () => {
 };
 
 preloadImages();
-
-/* function changeNumber() {
-    console.log("Heipä hei");
-} */
